0f7fb6f99182e50eb4d8863c13dcd9e8ce12f5fc
thomas-deneux <thomas.deneux@fairfaxmedia.com.au>
2016-02-05 10:12:01 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,7 @@ require 'mechanize'
 require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
 agent = Mechanize.new
-doc = agent.get("http://pdonline.scenicrim.qld.gov.au/Common/Common/Terms.aspx")
+doc = agent.get("http://daponline.scenicrim.qld.gov.au/Common/Common/Terms.aspx")
 form = doc.forms.first
 # Tick the box.
 id = doc.search('input')[4].attribute('name').value
@@ -11,4 +11,4 @@ form.checkbox_with(:name => id).check
 doc = doc.forms.first.submit(doc.forms.first.button_with(:value => "I Agree"), "Accept-Encoding" => "identity")
 
 # Use the ICON XML scraper.
-scrape_icon_rest_xml("http://pdonline.scenicrim.qld.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=last14days&o=xml", false, agent)
+scrape_icon_rest_xml("http://daponline.scenicrim.qld.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=last14days&o=xml", false, agent)
