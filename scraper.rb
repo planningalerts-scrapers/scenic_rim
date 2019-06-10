7d86fc80416dc3d5cda4b6edb16d1adaf0ce2f4a
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 15:54:00 +1000

    Use the latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,3 @@
 require "icon_scraper"
 
-agent = Mechanize.new
-doc = agent.get("https://srr-prod-icon.saas.t1cloud.com/Common/Common/Terms.aspx")
-form = doc.forms.first
-# Tick the box.
-form.checkbox_with(:name => 'ctl00$ctMain$chkAgree$chk1').check
-# Click the button.
-doc = doc.forms.first.submit(doc.forms.first.button_with(:value => "I Agree"), "Accept-Encoding" => "identity")
-
-# Use the ICON XML scraper.
-IconScraper.rest_xml("https://srr-prod-icon.saas.t1cloud.com/Pages/XC.Track/SearchApplication.aspx", "d=last28days&o=xml", false, agent)
+IconScraper.scrape_and_save(:scenic_rim)
